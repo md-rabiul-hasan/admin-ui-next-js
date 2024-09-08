ba998d99b60879e2e9166c64e62a3c75efdb36ff
Md.Rabiul Hasan <70057377+md-rabiul-hasan@users.noreply.github.com>
2024-09-08 16:03:18 +0600

    menu var design

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,28 @@ const menuItems = [
     ],
   },
 ];
+
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {
+        menuItems.map( menu => (
+          <div className="flex flex-col gap-2" key={menu.title}>
+              <span className="hidden lg:block font-bold my-4 text-gray-400">{menu.title}</span>
+              {
+                menu.items.map(item => (
+                  <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start text-gray-500 my-2 gap-2 ">
+                    <Image src={item.icon} alt="icon" width={20} height={20} />
+                    <span className="hidden lg:block">{item.label}</span>
+                  </Link>
+                ))
+              }
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Menu
