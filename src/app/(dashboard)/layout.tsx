f2cd21eaf061f9027a7a3668eb2bc45a246518d6
Md.Rabiul Hasan <70057377+md-rabiul-hasan@users.noreply.github.com>
2024-09-08 18:00:17 +0600

    navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,6 +4,7 @@ import "./../globals.css";
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,12 +24,15 @@ export default function DashboardLayout({
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
           <Image src="/logo.png" alt="Logo" width={32} height={32} />
-          <span className="hidden lg:block">Our School</span>
+          <span className="hidden lg:block font-extrabold text-gray-400">Our School</span>
         </Link>
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200"></div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
